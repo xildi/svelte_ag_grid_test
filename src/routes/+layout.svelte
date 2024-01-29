1d98097148ab97a17bfa08fff89ac506b91dd990
Georg Tomasini <gt@xildi.com>
2024-01-29 14:22:16 +0100

    added img

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,27 @@
-<slot/>
+<script lang="ts">    
+</script>
+
+<div id="content">
+    <img
+    height="50vh"
+    src="/img/Logo_dunkel.svg"
+    class="attachment-full size-full wp-image-27"
+    alt="Xildi Logo - More than numbers"
+/>
+    <div id="page">
+        <slot />
+    </div>
+</div>
 
 
 <style>
-     :global(body) {
-         background: #222628;
-         min-height: 100vh;
+    :global(body, html) {
+        background: #222628;
+    }
+    #content {
+        height: 100vh;
+    }
+    #page {
+        height: 100%;
     }
 </style>
